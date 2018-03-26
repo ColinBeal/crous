cfc22e464fcd373c08ca542cfc86d780c2562284
MystyH <boissymichael@gmail.com>
2018-03-26 12:55:56 +0200

    change

--- tableauAdmin.php
+++ tableauAdmin.php
@@ -4,16 +4,10 @@
     $result = mysqli_query($conn,$query);
     echo '<table class="table"><thead><tr><th>Etudiant</th><th>Date de naissance</th><th>Email</th></tr></thead><tbody>';
 
-<<<<<<< HEAD
     if (mysqli_num_rows($result) > 0)
     {
       while($row = $result->fetch_assoc())
       {
-=======
-    while($row = mysqli_fetch_assoc($result))
-    {
-      
->>>>>>> 2c67de03a1803a2dbefc340ea1616297ba106535
         echo "<tr><td>".$row['prenom']." ".$row['nom']."</td><td>".$row['date de naissance']."</td><td>".$row['mail']."</td></tr>";
       }
     }
